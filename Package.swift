da6fa218c6f7656caafe6422dba5571c10a72cd0
Saleem Abdulrasool <compnerd@compnerd.org>
2021-04-17 18:19:20 -0700

    build: add `Sample` product
    Add the `Sample` product to allow execution of the test program using
    `swift run`.

--- Package.swift
+++ Package.swift
@@ -9,6 +9,10 @@ let package = Package(
             name: "Backtrace",
             targets: ["Backtrace"]
         ),
+        .executable(
+          name: "Sample",
+          targets: ["Sample"]
+        )
     ],
     dependencies: [],
     targets: [
